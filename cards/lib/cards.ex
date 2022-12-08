e1f8de2ba3c963806c13071f05df0c6d0d5b9f8d
salmamyassinn <syassin@uplandsoftware.com>
2022-12-08 14:36:44 +0200

    Adding save and load

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -14,7 +14,24 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename,binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "That file does not exist"
+    end
   end
 end
