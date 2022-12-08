fd0421735e12540424bb76167c0102122af01436
salmamyassinn <syassin@uplandsoftware.com>
2022-12-09 01:58:52 +0200

    doc testing

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -2,6 +2,10 @@ defmodule Cards do
   @moduledoc """
     Provides methods for creating and handling a deck of cards
   """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
 
@@ -16,6 +20,18 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Returns a tuple with the hand and the rest of cards
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` is the number of cards in hand
+
+  ## Examples
+
+        iex> deck = Cards.create_deck
+        iex> {hand, deck} =  Cards.deal(deck, 1)
+        iex> hand
+        ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
